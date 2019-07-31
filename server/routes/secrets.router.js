357c0c222c6c8ff973fcfc1d90d81bca57d2e69e
Tim Heck <tih99924@bethel.edu>
2019-07-31 14:27:07 -0500

    rejectUnauthenticated added to secrets routes, checking clearance_level

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const pool = require('../modules/pool');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
+    pool.query(`SELECT * FROM "secret" WHERE secrecy_level < ${req.user.clearance_level};`)
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
